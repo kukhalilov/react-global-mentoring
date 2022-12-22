e37115f8fac4126ff0bd21d7e125b6c6943f3ade
Kuvonchbek Khalilov <kukhalilov@gmail.com>
2022-12-22 06:48:51 +0500

    feat: implement suggestions

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>Hello world!</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam non,
-        debitis quidem eveniet unde omnis modi? Nulla delectus voluptates,
-        eveniet molestias voluptatum quos officiis hic. Fuga animi perspiciatis
-        obcaecati! Necessitatibus.
-      </p>
-    </div>
-  );
-}
+const style: React.CSSProperties = {
+  textAlign: 'center',
+};
+
+const App = () => (
+  <div style={style}>
+    <h1>Hello world!</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam non, debitis
+      quidem eveniet unde omnis modi? Nulla delectus voluptates, eveniet
+      molestias voluptatum quos officiis hic. Fuga animi perspiciatis obcaecati!
+      Necessitatibus.
+    </p>
+  </div>
+);
 
 export default App;
